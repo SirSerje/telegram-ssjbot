ac7d4c12ada91bc8642c8eed7cbde7b767cf9654
sir.serje <sir.serje@gmail.com>
2018-05-07 01:39:37 +0300

    make pair function

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Bot = require('node-telegram-bot-api');
 const request = require('request');
-
 const token = "465330842:AAGl8YjnzejMHgPaUIbhhPJ8UpAYKOoxK-M";
-const url = 'http://sirserje.surge.sh/';
-const trigger = 'I want to travel!';
 
 const bot = new Bot(token, {polling: true});
-bot.onText(/answer/, msg => {
-    console.log(msg)
 
-    bot.sendMessage(msg.chat.id, 'Ты получил ответ?')
-})
+function askAnswer(message, answer) {
+    bot.onText(message, msg => {
+        //console.log(msg);
+        bot.sendMessage(msg.chat.id, answer);
+    })
+}
+
+askAnswer(/[пП]ривет/,`Привет!`);
+askAnswer(/[Hh]ello/,`Hello!`);
+askAnswer(/погода/,`Не знаю, как за окном, но в душе будет солнечно`);
